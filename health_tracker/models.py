43dc6333dbad192448bf53a5d90019d41f23148d
gina iliff <Giliff@me.com>
2017-04-26 22:13:49 -0700

    set up tables

--- health_tracker/models.py
+++ health_tracker/models.py
@@ -3,4 +3,19 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class Drink(models.Model):
+    drink_type = models.CharField(max_length=50)  # drink name
+    drink_size = models.IntegerField(default=0)  # in ounces
+    drink_calories = models.IntegerField(default=0)  # estimated calories, if applicable
+
+
+class Food(models.Model):
+    food_type = models.CharField(max_length=100)  # food name
+    food_calories = models.IntegerField(default=0)  # estimated calorie count
+
+
+class Activity(models.Model):
+    activity_type = models.CharField(max_length=50)  # name of activity
+    activity_time = models.IntegerField(default=0)  # minutes of activity
+    activity_distance = models.FloatField(default=0.0)  # if applicable, how many miles
