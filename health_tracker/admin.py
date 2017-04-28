9a2a34e8b4f92abee336fe23c77147149ee2d7aa
gina iliff <Giliff@me.com>
2017-04-28 13:51:16 -0700

    set up admin, fixed models

--- health_tracker/admin.py
+++ health_tracker/admin.py
@@ -3,4 +3,8 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-# Register your models here.
+from .models import Drink, Food, Activity
+
+admin.site.register(Drink)
+admin.site.register(Food)
+admin.site.register(Activity)
--- health_tracker/models.py
+++ health_tracker/models.py
@@ -2,20 +2,36 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
 
 
+@python_2_unicode_compatible
 class Drink(models.Model):
     drink_type = models.CharField(max_length=50)  # drink name
     drink_size = models.IntegerField(default=0)  # in ounces
     drink_calories = models.IntegerField(default=0)  # estimated calories, if applicable
+    date_added = models.DateTimeField('date added')
+
+    def __str__(self):
+        return self.drink_type
 
 
+@python_2_unicode_compatible
 class Food(models.Model):
     food_type = models.CharField(max_length=100)  # food name
     food_calories = models.IntegerField(default=0)  # estimated calorie count
+    date_added = models.DateTimeField('date added')
+
+    def __str__(self):
+        return self.food_type
 
 
+@python_2_unicode_compatible
 class Activity(models.Model):
     activity_type = models.CharField(max_length=50)  # name of activity
     activity_time = models.IntegerField(default=0)  # minutes of activity
     activity_distance = models.FloatField(default=0.0)  # if applicable, how many miles
+    date_added = models.DateTimeField('date added')
+
+    def __str__(self):
+        return self.activity_type
